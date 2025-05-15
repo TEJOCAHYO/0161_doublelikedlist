95a75bcdccbf24e65bdba5727632b587618dda6e
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-05-15 09:45:11 +0700

    menambahkan library

---
+++ DoblyLingkedList.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+
+};
+
+class DoubleLingkedList
+{
+private:
+    Node *START
+}
