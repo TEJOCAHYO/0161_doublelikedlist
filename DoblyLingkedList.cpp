783126407ee9ff46ce52a5671b67e47cee5cdd9e
TEJOCAHYO <tejo.cahyo.ft24@mail.umy.ac.id>
2025-05-15 10:17:26 +0700

    manambahkan class doublelingketlist

--- DoblyLingkedList.cpp
+++ DoblyLingkedList.cpp
@@ -1,18 +1,70 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
-class Node
-{
-public:
+class Node {
+    public:
     int noMhs;
     Node *next;
     Node *prev;
-
 };
 
-class DoubleLingkedList
-{
+class DoubleLinkedList {
 private:
-    Node *START
+    Node *START;
+
+public:
+    DoubleLinkedList(){
+        START = NULL;
+    }
+
+    void addNode(){
+        int nim;
+        string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        // Step 1: Allocate memory for a new node
+        Node *newNode = new Node();
+        
+        // Step 2: Assign value to the data fields
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs){
+            if (START != NULL && nim == START->noMhs){
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // Step 4: newNode.next = START;
+            newNode->next = START;
+
+            // Step 5: START.prev = newNode (if START exists)
+            if (START != NULL) 
+                START->prev = newNode;
+            
+            // Step 6: newNode.prev = NULL
+            newNode->prev = NULL;
+
+            // Step 7: START = newNode
+            START = newNode;
+            return;
+        }
+
+        //Insert in between Node
+        //Step 8: Locate position for inertion
+        Node *current = START;
+        while (current->next != NULL && current ->next->noMhs < nim){
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs){
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+    }
+};
+
+
+int main (){
+    
 }
